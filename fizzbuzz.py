2a273d82182c13d5348a51ba61ee4faa6d106448
moise degboe <degboe@crit-xubuntu>
2023-10-02 15:20:46 +0200

    new commit 2

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,3 @@
 class fizzbuzz:
     def affiche (self):
-        return "12"
+        return "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FrisBee7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz"
